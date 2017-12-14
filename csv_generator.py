14448832f7124806419587a8bbc5814ae69a69d3
Ubuntu <ubuntu@ip-172-31-1-67.us-west-1.compute.internal>
2017-12-14 06:27:10 +0000

    move everything to a class add the api which generates csv file

--- csv_generator.py
+++ csv_generator.py
@@ -1,12 +1,30 @@
+import csv
+import json
 import os
 import requests
+import tempfile
 
-GOOGLE_PLACES_API = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
-PALCES_QUERY_PARAMS = {
-    'key': os.environ['GOOGLE_API_KEY'],
-    'location': '37.7749,-122.4194',
-    'radius': 1000,
-    'type': 'restaurant',
-}
+class CSVGenerator:
+    GOOGLE_PLACES_API = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+    PALCES_QUERY_PARAMS = {
+        'key': os.environ['GOOGLE_API_KEY'],
+        'location': '37.7749,-122.4194',
+        'radius': 1000,
+        'type': 'restaurant',
+    }
 
-print requests.get(GOOGLE_PLACES_API, params=PALCES_QUERY_PARAMS).content
+    def generate_csv(self):
+        places_response = requests.get(self.GOOGLE_PLACES_API, params=self.PALCES_QUERY_PARAMS).content
+        places = json.loads(places_response)
+        with tempfile.NamedTemporaryFile(delete=False) as f:
+            csvwriter = csv.writer(f)
+            csvwriter.writerow(['Restaurant name', 'Rating'])
+            for place in places['results']:
+                if place['rating'] > 3:
+                    csvwriter.writerow([place['name'].encode("utf-8"), place['rating']])
+
+        return f.name
+
+if __name__ == '__main__':
+    c = CSVGenerator()
+    print (c.generate_csv())
